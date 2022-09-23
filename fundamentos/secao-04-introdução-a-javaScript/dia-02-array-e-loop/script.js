f2f1602773f09895a43adfafedd4d4722e3b334c
cinho <cinho@pop-os.localdomain>
2022-09-23 12:54:38 -0300

    aritimetica do array e exibição com if

--- fundamentos/secao-04-introdução-a-javaScript/dia-02-array-e-loop/script.js
+++ fundamentos/secao-04-introdução-a-javaScript/dia-02-array-e-loop/script.js
@@ -3,7 +3,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log(number);
 // }
 
+let sum = 0;
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index]);
-  
+  sum = sum + numbers[index];
+}
+
+let aritimetica = sum / numbers.length;
+console.log(sum);
+
+if (aritimetica > 20) {
+  console.log("Valor maior que 20");
+} else {
+  console.log("Valor menor ou igual a 20")
 }

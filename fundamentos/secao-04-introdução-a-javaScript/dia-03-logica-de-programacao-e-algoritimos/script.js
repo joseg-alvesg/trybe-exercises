ec9cc7397ab2fa4b16ad292e6f2a30ba12fb780f
cinho <cinho@pop-os.localdomain>
2022-09-24 16:02:52 -0300

    resolução da inversão de palavras

--- fundamentos/secao-04-introdução-a-javaScript/dia-03-logica-de-programacao-e-algoritimos/script.js
+++ fundamentos/secao-04-introdução-a-javaScript/dia-03-logica-de-programacao-e-algoritimos/script.js
@@ -1,3 +1,6 @@
+//=============================================================
+// Calculo de fatorial
+//=============================================================
 let fatorial = 10;
 let resultado = fatorial;
 
@@ -5,3 +8,15 @@ for (let index = 1; index < fatorial; index += 1) {
   resultado *= index;
 }
 console.log(resultado);
+
+//=============================================================
+// inversão de palavras
+//=============================================================
+
+let word = 'o rato roeu a roupa do rei de ratos';
+let inversor = [];
+
+for (index = 0; index < word.length; index += 1){
+  inversor.unshift(word[index]);
+}
+console.log(inversor.join(''));

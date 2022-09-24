b9e6849fd4cf33305e42cd28c97e1c354bb196f0
cinho <cinho@pop-os.localdomain>
2022-09-24 20:04:45 -0300

    maior e menor palavra do array

--- fundamentos/secao-04-introdução-a-javaScript/dia-03-logica-de-programacao-e-algoritimos/script.js
+++ fundamentos/secao-04-introdução-a-javaScript/dia-03-logica-de-programacao-e-algoritimos/script.js
@@ -22,3 +22,22 @@ for (index = word.length-1; index >= 0; index -= 1){
 // console.log(inversor.join(''));
 console.log(inversor)
 
+//=============================================================
+// Maior e menor palavra do array
+//=============================================================
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+maiorPalavra = array[0];
+menorPalavra = array[0];
+
+for (let index = 1; index < array.length; index += 1) {
+  if (maiorPalavra.length < array[index].length) {
+    maiorPalavra = array[index];
+  }
+  
+  if (menorPalavra.length > array[index].length){
+    menorPalavra = array[index];
+  }
+}
+console.log(maiorPalavra);
+console.log(menorPalavra);

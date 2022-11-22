d99873d8eddce8b0734df8f0f1c86bcc7c720fc6
joseg-alvesg <cinhoalves85@gmail.com>
2022-11-22 20:57:48 -0300

    exercicio de fixação

---
+++ front-end/dia-01-introducao-react/exercicio-fixacao/src/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>José<p>Sou Desenvolvedor web!</p></h1>
+        <h2>
+          Minhas Habilidades
+          <ol>
+            <li>Hofs</li>
+            <li>Criatividade</li>
+            <li>Requisições</li>
+          </ol>
+        </h2>
+      </main>
+    )
+  }
+}
+
+export default About;
